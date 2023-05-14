5c1ad79dec955f8886c1d090a4f1e16283cbba3f
erk1nqz <221326@astanait.edu.kz>
2023-05-14 21:58:52 +0600

    MyTestingClass class

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,2 +1,16 @@
-public class MyTestingClass {
+class MyTestingClass {
+    private int key;
+
+    public MyTestingClass(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    @Override
+    public int hashCode() {
+        return key;
+    }
 }
